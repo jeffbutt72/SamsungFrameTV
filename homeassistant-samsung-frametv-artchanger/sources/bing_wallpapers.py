00605d6380174f7d4de7505811a6746597557d40
Jeff Butt <jbutt@twnc.com>
2026-04-05 16:55:30 -0400

    Update bing_wallpapers.py

--- homeassistant-samsung-frametv-artchanger/sources/bing_wallpapers.py
+++ homeassistant-samsung-frametv-artchanger/sources/bing_wallpapers.py
@@ -1,25 +1,14 @@
-import logging
-import random
 import requests
 from io import BytesIO
-from datetime import datetime, timedelta
-from typing import Tuple, Optional, List, Dict
 
-def get_image_url(args):
-    # wallpapers before 2021-08-28 are not available in 4k from https://bing.npanuhin.me/US/en/2021-08-28.jpg
-    start_date: datetime = datetime(2021, 8, 28)
-    end_date: datetime = datetime.now()
-    random_date: datetime = start_date + timedelta(days=random.randint(0, (end_date - start_date).days))
-    formatted_date: str = random_date.strftime("%Y-%m-%d")
-    url: str = f"https://bing.npanuhin.me/US/en/{formatted_date}.jpg"
-    return url
+def get_image_url():
+    return "https://bing.biturl.top/?resolution=UHD&mkt=en-CA"
 
-def get_image(args, url) -> Tuple[Optional[BytesIO], Optional[str]]:
+def get_image(url):
     try:
-        response: requests.Response = requests.get(url)
-        response.raise_for_status()
-        image_data: BytesIO = BytesIO(response.content)
-        return image_data, "JPEG"
-    except requests.RequestException as e:
-        logging.error(f"Failed to fetch Bing Wallpaper: {str(e)}")
+        r = requests.get(url)
+        r.raise_for_status()
+        return BytesIO(r.content), "JPEG"
+    except Exception as e:
+        print(f"Error: {e}")
         return None, None
